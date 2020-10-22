e17e532d01376cb6041588fabb8c17e8cf603de6
ychloe <ychloe9252@gmail.com>
2020-10-21 20:11:37 -0400

    data cleaning script created

--- glassdoor_scraper.py
+++ glassdoor_scraper.py
@@ -7,6 +7,11 @@ from selenium import webdriver
 import time
 import pandas as pd
 
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 9354d7c99f94dec23526a7b6901c80e8443bc2e4
 def get_jobs(keyword, num_jobs, verbose,path,slp_time):
     
     '''Gathers jobs as a dataframe, scraped from Glassdoor'''
@@ -48,9 +53,58 @@ def get_jobs(keyword, num_jobs, verbose,path,slp_time):
         #Going through each job in this page
         job_buttons = driver.find_elements_by_class_name("jl")  #jl for Job Listing. These are the buttons we're going to click.
         for job_button in job_buttons:  
+<<<<<<< HEAD
                 print("Progress: {}".format("" + str(len(jobs)) + "/" + str(num_jobs)))
                 if len(jobs) >= num_jobs:
                     break 
+=======
+            print("Progress: {}".format("" + str(len(jobs)) + "/" + str(num_jobs)))
+            if len(jobs) >= num_jobs:
+                break
+            try:
+                driver.execute_script("arguments[0].click();", job_button)
+            except NoSuchElementException:
+                print("invalid button")
+                pass
+            time.sleep(1)
+            collected_successfully = False
+            
+            while not collected_successfully:
+                try:
+                    company_name = driver.find_element_by_xpath('.//div[@class="employerName"]').text
+                    location = driver.find_element_by_xpath('.//div[@class="location"]').text
+                    job_title = driver.find_element_by_xpath('.//div[contains(@class, "title")]').text
+                    job_description = driver.find_element_by_xpath('.//div[@class="jobDescriptionContent desc"]').text
+                    collected_successfully = True
+                except:
+                    time.sleep(5)
+
+            try:
+                salary_estimate = driver.find_element_by_xpath('.//div[@class="salary"]').text
+            except NoSuchElementException:
+                salary_estimate = -1 #You need to set a "not found value. It's important."
+            
+            try:
+                rating = driver.find_element_by_xpath('.//span[@class="rating"]').text
+            except NoSuchElementException:
+                rating = -1 #You need to set a "not found value. It's important."
+
+            #Printing for debugging
+            if verbose:
+                print("Job Title: {}".format(job_title))
+                print("Salary Estimate: {}".format(salary_estimate))
+                print("Job Description: {}".format(job_description[:500]))
+                print("Rating: {}".format(rating))
+                print("Company Name: {}".format(company_name))
+                print("Location: {}".format(location))
+
+            #Going to the Company tab...
+            #clicking on this:
+            #<div class="tab" data-tab-type="overview"><span>Company</span></div>
+            try:
+                driver.find_element_by_xpath('.//div[@class="tab" and @data-tab-type="overview"]').click()
+                print("company tab work")
+>>>>>>> 9354d7c99f94dec23526a7b6901c80e8443bc2e4
                 try:
                     driver.execute_script("arguments[0].click();", job_button)
                 except NoSuchElementException:
@@ -143,6 +197,7 @@ def get_jobs(keyword, num_jobs, verbose,path,slp_time):
                     sector = -1
                     revenue = -1
                     competitors = -1
+<<<<<<< HEAD
                     print("no company tab")
                     
                 if verbose:
@@ -175,6 +230,49 @@ def get_jobs(keyword, num_jobs, verbose,path,slp_time):
         #Clicking on the "next page" button
         time.sleep(4)
         
+=======
+
+            except NoSuchElementException:  #Rarely, some job postings do not have the "Company" tab.
+                headquarters = -1
+                size = -1
+                founded = -1
+                type_of_ownership = -1
+                industry = -1
+                sector = -1
+                revenue = -1
+                competitors = -1
+                print("no company tab")
+                
+            if verbose:
+                print("Headquarters: {}".format(headquarters))
+                print("Size: {}".format(size))
+                print("Founded: {}".format(founded))
+                print("Type of Ownership: {}".format(type_of_ownership))
+                print("Industry: {}".format(industry))
+                print("Sector: {}".format(sector))
+                print("Revenue: {}".format(revenue))
+                print("Competitors: {}".format(competitors))
+                print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+            jobs.append({"Job Title" : job_title,
+            "Salary Estimate" : salary_estimate,
+            "Job Description" : job_description,
+            "Rating" : rating,
+            "Company Name" : company_name,
+            "Location" : location,
+            "Headquarters" : headquarters,
+            "Size" : size,
+            "Founded" : founded,
+            "Type of ownership" : type_of_ownership,
+            "Industry" : industry,
+            "Sector" : sector,
+            "Revenue" : revenue,
+            "Competitors" : competitors})
+            #add job to jobs
+
+        #Clicking on the "next page" button
+        time.sleep(4)
+>>>>>>> 9354d7c99f94dec23526a7b6901c80e8443bc2e4
         try:
             next_button=driver.find_element_by_xpath('.//li[@class="next"]//a');
             driver.execute_script("arguments[0].click();", next_button)
--- glassdoor_scraper_execution.py
+++
@@ -1,5 +0,0 @@
-import glassdoor_scraper as gs
-import pandas as pd
-
-data=gs.get_jobs("data scientist",1000,False,"C:/Users/chloe/Documents/ds_salary_proj/chromedriver.exe" ,15)
-data.to_csv('glassdoor_jobs.csv',index=False)
